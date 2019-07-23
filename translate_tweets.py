00725394c32375e52803a29e5f96457293587b0d
jh <dev1@correo.com>
2019-07-22 19:09:03 -0500

    primer subida al servidor

---
+++ translate_tweets.py
@@ -0,0 +1,108 @@
+from textblob import TextBlob
+import numpy as np
+
+
+class TranslateTweets():
+    def __init__(self, array_of_tweets_translated=[], array_of_tweets_and_score=[], tweets_arr = []):
+        self.array_of_tweets_translated = []
+        self.array_of_tweets_and_score = []
+        self.tweets_arr = []
+    
+    def get_full_text_tweet(self, tweets):
+        for tweet_info in tweets:
+            if 'retweeted_status' in dir(tweet_info):
+                pass
+            else:
+                tweet = tweet_info.full_text
+                self.tweets_arr.append(tweet)
+        return self.tweets_arr
+
+    def translate_tweets(self, tweets):
+        #array_of_tweets = np.array([tweet.full_text for tweet in tweets])
+        for tweet in tweets:
+            t = TextBlob(tweet)
+            ten = t.translate(to="en")
+            self.array_of_tweets_translated.append(ten.sentiment[0])
+            self.array_of_tweets_and_score.append({"text": tweet, "score": ten.sentiment[0]})
+        return self.array_of_tweets_translated
+
+    def array_of_tweets_and_score_method(self):
+        return self.array_of_tweets_and_score
+    
+    def order_array_of_tweets_neutral(self, array_tweets_score):
+        array_neutral = []
+        for element in array_tweets_score:
+            if element['score'] == 0:
+                array_neutral.append(element)
+        return array_neutral
+
+    def order_array_of_tweets_postive(self, array_tweets_score):
+        array_positive = []
+        for element in array_tweets_score:
+            if element['score'] > 0:
+                array_positive.append(element)
+        return array_positive
+
+    def order_array_of_tweets_negative(self, array_tweets_score):
+        array_negative = []
+        for element in array_tweets_score:
+            if element['score'] < 0:
+                array_negative.append(element)
+        return array_negative
+    
+    def text_neutral_only(self, array_neutral):
+        text_neutral = [comment['text'] for comment in array_neutral]
+        return text_neutral
+  
+    def text_positive_only(self, array_positive):
+        text_positive = [comment['text'] for comment in array_positive]
+        return text_positive
+
+    def text_negative_only(self, array_negative):
+        text_negative = [comment['text'] for comment in array_negative]
+        return text_negative
+
+    def delete_zeros(self):
+        new_score_list = []
+        for score in self.array_of_tweets_translated:
+            if score == 0:
+                pass
+            else:
+                new_score_list.append(score)
+        return new_score_list
+  
+    def calculate_percentage_neutral(self):
+        neutral_scores = [score for score in self.array_of_tweets_translated if score == 0]
+        neutral_percentage = (len(neutral_scores) * 100 / len(self.array_of_tweets_translated))
+        return neutral_percentage
+        """ total = len(self.tweets_arr)
+        neutral = []
+        for score in self.array_of_tweets_translated:
+            if score == 0:
+                neutral.append(score)
+        neutral_result = ((len(neutral)) * 100 / total)
+        return neutral_result """
+  
+    def calculate_percentage_positive(self):
+        positive_scores = [score for score in self.array_of_tweets_translated if score > 0]
+        positive_percentage = (len(positive_scores) * 100 / len(self.array_of_tweets_translated))
+        return positive_percentage
+        """ total = len(self.tweets_arr)
+        positive = []
+        for score in self.array_of_tweets_translated:
+            if score > 0:
+                positive.append(score)
+        positive_result = ((len(positive)) * 100 / total)
+        return positive_result """
+
+    def calculate_percentage_negative(self):
+        negative_scores = [score for score in self.array_of_tweets_translated if score < 0]
+        negative_percentage = (len(negative_scores) * 100 / len(self.array_of_tweets_translated))
+        return negative_percentage
+        """ total = len(self.tweets_arr)
+        negative = []
+        for score in self.array_of_tweets_translated:
+            if score < 0:
+                negative.append(score)
+        negative_result = ((len(negative)) * 100 / total)
+        return negative_result """
---
+++ webapp.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from translate_tweets import TranslateTweets
+from twitter_client import TwitterAuthenticator
+import tweepy
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+
+app = Flask(__name__)
+
+# settings
+app.secret_key = 'mysecretkey'
+
+####input your credentials here
+consumer_key = 'wtFRuCKCv8uB4zchxPpj0IxF7'
+consumer_secret = 'SUPHCNtK7QWe8fKpYwPt11CKQ9vWdGwawrrMaItEFIuuSAe9d1'
+access_token = '2205718165-0nE4pGg3XxLVDQztEGdKmmiXfOkFBRPF4D7SDTm'
+access_token_secret = 'n9HnBZgoH1gXUWEo8KGYepYkNHi9lyJNe3hnfEvmxxy67'
+
+twitter_authenticator = TwitterAuthenticator(consumer_key, consumer_secret, access_token, access_token_secret)
+api = twitter_authenticator.twitterClient()
+   
+
+@app.route('/')
+def home():   
+    #tweets_translated = TranslateTweets()
+    #tweets_translated_array = tweets_translated.translate_tweets(tweets)
+    return render_template('search.html')
+
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+    if request.method == 'POST':
+        percentages = []
+        quantities = []
+        term = request.form['term']
+        term = str(term)
+        tweets = tweepy.Cursor(api.search, q=term, lang = 'es', tweet_mode='extended').items(100)
+        tweets_translated = TranslateTweets()
+        tweets_full_text = tweets_translated.get_full_text_tweet(tweets)
+        tweets_translated_array = tweets_translated.translate_tweets(tweets_full_text)
+        array_tweets_score = tweets_translated.array_of_tweets_and_score_method()
+        array_neutral = tweets_translated.order_array_of_tweets_neutral(array_tweets_score)
+        array_positive = tweets_translated.order_array_of_tweets_postive(array_tweets_score)
+        array_negative = tweets_translated.order_array_of_tweets_negative(array_tweets_score)
+        text_neutral = tweets_translated.text_positive_only(array_neutral)
+        text_positive = tweets_translated.text_positive_only(array_positive)
+        text_negative = tweets_translated.text_negative_only(array_negative)
+        array_without_zeros = tweets_translated.delete_zeros()
+        percentage_neutral = tweets_translated.calculate_percentage_neutral()
+        percentage_positive = tweets_translated.calculate_percentage_positive()
+        percentage_negative = tweets_translated.calculate_percentage_negative()
+        neutral_quantity = len(array_neutral)
+        positive_quantity = len(array_positive)
+        negative_quantity = len(array_negative)
+        percentages.extend([percentage_neutral, percentage_positive, percentage_negative])
+        quantities.extend([neutral_quantity, positive_quantity, negative_quantity])
+        data = [{"text_neutral": text_neutral}, {"text_positive": text_positive}, {"text_negative": text_negative}, {"percentages": percentages}, {"quantities": quantities}]
+        scores_array = np.array(array_without_zeros)
+        sns.set()
+        ax = sns.distplot(scores_array)
+        plt.show()
+        return render_template('show.html', data = data)
+
+if __name__ == '__main__':
+    app.run(port = 4000, debug=True)
+    
